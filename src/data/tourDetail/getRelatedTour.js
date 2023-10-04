44eb1b4db5b595c1247721cbae9716de0367630b
daithehh04 <daithehh04@gmail.com>
2023-10-04 07:12:29 +0700

    update time

--- src/data/tourDetail/getRelatedTour.js
+++ src/data/tourDetail/getRelatedTour.js
@@ -13,7 +13,7 @@ export default async function getRelatedTour(taxonomyValue, taxonomyName, lang) 
         variables: { taxonomyValue: taxonomyValue, taxonomyName: taxonomyName, language: lang?.toUpperCase() }
       })
     },
-    // { next: { revalidate: 60 } }
+    { next: { revalidate: 60 } }
   )
   if (!res.ok) {
     throw new Error('Failed to fetch data')
