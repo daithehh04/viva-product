74c3b63d37a5879e5e5ee9c5c3f9a7affea1a66a
daithehh04 <daithehh04@gmail.com>
2023-10-16 10:01:40 +0700

    fix news related

--- src/pageComponent/Search/NewRelease.js
+++ src/pageComponent/Search/NewRelease.js
@@ -7,11 +7,13 @@ const NewRelease = ({ data, lang, title, button }) => {
     <div>
       <div className='mt-[6.69vw]'>
         <h2 className='mb-[3vw] font-optima font-semibold leading-[4.4vw] capitalize text-[4vw] max-md:px-[4.27vw]'>{title || 'Related News'}</h2>
-        <BestSeller
-          isBlogItem={true}
-          listBlog={data}
-          lang={lang}
-        />
+        <div className='related-news__search'>
+          <BestSeller
+            isBlogItem={true}
+            listBlog={data}
+            lang={lang}
+          />
+        </div>
       </div>
       <div className='flex md:mt-[4vw] mt-[9.87vw] md:mb-[5.75vw] mb-[14.93vw] justify-center'>
         <Button className='btn-secondary' content={button}><span>{button}</span></Button>
